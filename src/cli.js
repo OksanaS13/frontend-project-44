c772c38383e7ba1a9ca582e84dfe36ba718a5ff6
OksanaS13 <oksan4ik_96@mail.ru>
2022-11-20 18:36:23 +0300

    update src/cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,8 @@
 import readlineSync from 'readline-sync';
 
-const findOutTheName = () => {
+const findOutName = () => {
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
 };
 
-export default findOutTheName;
+export default findOutName;
