b9ccbe4ff8e8aeed2e828b96a325b62d5303db44
OksanaS13 <oksan4ik_96@mail.ru>
2022-11-20 18:36:47 +0300

    update src/index.js

--- src/index.js
+++ src/index.js
@@ -5,18 +5,27 @@ const startTheGame = (instruction) => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
+  const roundsCount = 3;
   console.log(instruction);
-  return name;
+  return [name, roundsCount];
 };
 
-const brainGames = (currentQuestion, correctAnswer) => {
+const playBrainGames = (currentQuestion, correctAnswer, roundCounter, name) => {
+  let flag = true;
   let userAnswer = readlineSync.question(`Question: ${currentQuestion}
 Your answer: `);
   if (Number.isInteger(correctAnswer)) {
     userAnswer = Number(userAnswer);
   }
   const result = (userAnswer === correctAnswer) ? 'Correct!' : `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`;
-  return result;
+  console.log(result);
+  if (result !== 'Correct!') {
+    flag = false;
+    console.log(`Let's try again, ${name}!`);
+  } else if (roundCounter === 3) {
+    console.log(`Congratulations, ${name}!`);
+  }
+  return flag;
 };
 
-export { startTheGame, brainGames };
+export { startTheGame, playBrainGames };
