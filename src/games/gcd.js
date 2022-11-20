698f151545e41512bf5a38de0a57dd5d9fbc562a
OksanaS13 <oksan4ik_96@mail.ru>
2022-11-20 18:35:27 +0300

    update src/games/gcd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,47 +1,33 @@
-import _ from 'lodash';
-import { startTheGame, brainGames } from '../index.js';
+import { startTheGame, playBrainGames } from '../index.js';
+import getRandomNumber from '../utils.js';
 
 const playBrainGcd = () => {
   const instruction = 'Find the greatest common divisor of given numbers.';
-
-  const name = startTheGame(instruction);
+  const [name, roundsCount] = startTheGame(instruction);
 
   const findOutCorrectAnswer = (numberOne, numberTwo) => {
-    const divisorsOne = [];
-    const divisorsTwo = [];
+    const commonDivisors = [];
     const smallestNumber = Math.min(numberOne, numberTwo);
     for (let i = 1; i <= smallestNumber; i += 1) {
       if (numberOne % i === 0) {
-        divisorsOne.push(i);
-      }
-      if (numberTwo % i === 0) {
-        divisorsTwo.push(i);
+        if (numberTwo % i === 0) {
+          commonDivisors.push(i);
+        }
       }
     }
-    const commonDivisors = _.intersection(divisorsOne, divisorsTwo);
     const maxDivisor = Math.max(...commonDivisors);
     return maxDivisor;
   };
 
-  let flag = true;
-  const roundsCount = 3;
-
-  for (let i = 0; i < roundsCount; i += 1) {
-    const numberOne = Math.floor((Math.random() * 100) + 1);
-    const numberTwo = Math.floor((Math.random() * 100) + 1);
+  for (let i = 1; i <= roundsCount; i += 1) {
+    const numberOne = getRandomNumber();
+    const numberTwo = getRandomNumber();
     const question = `${numberOne} ${numberTwo}`;
     const correctAnswer = findOutCorrectAnswer(numberOne, numberTwo);
-    const result = brainGames(question, correctAnswer);
-    console.log(result);
-    if (result !== 'Correct!') {
-      flag = false;
-      console.log(`Let's try again, ${name}!`);
+    const flag = playBrainGames(question, correctAnswer, i, name);
+    if (flag === false) {
       break;
     }
-  }
-
-  if (flag === true) {
-    console.log(`Congratulations, ${name}!`);
   }
 };
 
