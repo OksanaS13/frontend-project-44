da074f8fd672b1d1c0b7e0b9bfc583afff389d03
OksanaS13 <oksan4ik_96@mail.ru>
2022-11-24 16:30:16 +0300

    update src/games/prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,32 +1,27 @@
-import { startTheGame, playBrainGames } from '../index.js';
+import playBrainGames from '../index.js';
 import getRandomNumber from '../utils.js';
 
 const playBrainPrime = () => {
   const instruction = 'Answer "yes" if given number is prime. Otherwise answer "no".';
-  const [name, roundsCount] = startTheGame(instruction);
+  const playRound = () => {
+    const question = getRandomNumber(100, 1);
 
-  const findOutCorrectAnswer = (number) => {
     let divisorsCount = 0;
-    if (number === 1) {
+    if (question < 2) {
       divisorsCount += 1;
     }
-    for (let i = 2; i <= Math.sqrt(number); i += 1) {
-      if (number % i === 0) {
+    for (let i = 2; i <= Math.sqrt(question); i += 1) {
+      if (question % i === 0) {
         divisorsCount += 1;
         break;
       }
     }
-    return (divisorsCount === 0) ? 'yes' : 'no';
+    const correctAnswer = (divisorsCount === 0) ? 'yes' : 'no';
+
+    return [question, correctAnswer];
   };
 
-  for (let i = 1; i <= roundsCount; i += 1) {
-    const question = getRandomNumber(100, 1);
-    const correctAnswer = findOutCorrectAnswer(question);
-    const flag = playBrainGames(question, correctAnswer, i, name);
-    if (flag === false) {
-      break;
-    }
-  }
+  playBrainGames(instruction, playRound);
 };
 
 export default playBrainPrime;
