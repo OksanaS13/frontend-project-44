0f39f8e4cc7583cd1ce2f15a8b3c652b3096fb26
OksanaS13 <oksan4ik_96@mail.ru>
2022-11-24 16:29:53 +0300

    update src/games/gcd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,37 +1,32 @@
-import { startTheGame, playBrainGames } from '../index.js';
+import playBrainGames from '../index.js';
 import getRandomNumber from '../utils.js';
 
 const playBrainGcd = () => {
   const instruction = 'Find the greatest common divisor of given numbers.';
-  const [name, roundsCount] = startTheGame(instruction);
-
-  const findOutCorrectAnswer = (numberOne, numberTwo) => {
-    const commonDivisors = [];
-    const smallestNumber = Math.min(numberOne, numberTwo);
-    if (smallestNumber === 0) {
-      return '-Infinity';
-    }
-    for (let i = 1; i <= smallestNumber; i += 1) {
-      if (numberOne % i === 0) {
-        if (numberTwo % i === 0) {
-          commonDivisors.push(i);
-        }
-      }
-    }
-    const maxDivisor = Math.max(...commonDivisors);
-    return maxDivisor;
-  };
-
-  for (let i = 1; i <= roundsCount; i += 1) {
+  const playRound = () => {
     const numberOne = getRandomNumber();
     const numberTwo = getRandomNumber();
     const question = `${numberOne} ${numberTwo}`;
-    const correctAnswer = findOutCorrectAnswer(numberOne, numberTwo);
-    const flag = playBrainGames(question, correctAnswer, i, name);
-    if (flag === false) {
-      break;
+
+    let correctAnswer;
+    const commonDivisors = [];
+    const smallestNumber = Math.min(numberOne, numberTwo);
+    if (smallestNumber === 0) {
+      correctAnswer = Math.max(numberOne, numberTwo);
+    } else {
+      for (let i = 1; i <= smallestNumber; i += 1) {
+        if (numberOne % i === 0) {
+          if (numberTwo % i === 0) {
+            commonDivisors.push(i);
+          }
+        }
+      }
+      correctAnswer = Math.max(...commonDivisors);
     }
-  }
+    return [question, String(correctAnswer)];
+  };
+
+  playBrainGames(instruction, playRound);
 };
 
 export default playBrainGcd;
