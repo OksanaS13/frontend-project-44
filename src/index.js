101eba17bf203eb1d5ff88c7c05efd54e1f704bc
OksanaS13 <oksan4ik_96@mail.ru>
2022-11-24 16:31:05 +0300

    update src/index.js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,25 @@
 import readlineSync from 'readline-sync';
 
-const startTheGame = (instruction) => {
+const playBrainGames = (instruction, playRound) => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
-  const roundsCount = 3;
   console.log(instruction);
-  return [name, roundsCount];
-};
-
-const playBrainGames = (currentQuestion, correctAnswer, roundCounter, name) => {
-  let flag = true;
-  let userAnswer = readlineSync.question(`Question: ${currentQuestion}
-Your answer: `);
-  if (Number.isInteger(correctAnswer)) {
-    userAnswer = Number(userAnswer);
+  const roundsCount = 3;
+  for (let i = 1; i <= roundsCount; i += 1) {
+    const [currentQuestion, correctAnswer] = playRound();
+    const userAnswer = String(readlineSync.question(`Question: ${currentQuestion}
+Your answer: `));
+    if (userAnswer === correctAnswer) {
+      console.log('Correct!');
+      if (i === roundsCount) {
+        console.log(`Congratulations, ${name}!`);
+      }
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${name}!`);
+      break;
+    }
   }
-  const result = (userAnswer === correctAnswer) ? 'Correct!' : `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`;
-  console.log(result);
-  if (result !== 'Correct!') {
-    flag = false;
-    console.log(`Let's try again, ${name}!`);
-  } else if (roundCounter === 3) {
-    console.log(`Congratulations, ${name}!`);
-  }
-  return flag;
 };
-
-export { startTheGame, playBrainGames };
+export default playBrainGames;
