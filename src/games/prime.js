1d9156531bbdfa82b0d7e0062c2911f7a8a5034c
OksanaS13 <oksan4ik_96@mail.ru>
2022-11-28 17:41:06 +0300

    update src/games/prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,28 +1,30 @@
 import playBrainGames from '../index.js';
 import getRandomNumber from '../utils.js';
 
+const isPrime = (number) => {
+  let divisorsCount = 0;
+  if (number < 2) {
+    divisorsCount += 1;
+  }
+  for (let i = 2; i <= Math.sqrt(number); i += 1) {
+    if (number % i === 0) {
+      divisorsCount += 1;
+      break;
+    }
+  }
+  return divisorsCount === 0;
+};
+
+const prepareRound = () => {
+  const question = getRandomNumber(1, 100);
+
+  const correctAnswer = (isPrime(question)) ? 'yes' : 'no';
+
+  return [question, correctAnswer];
+};
+
 const playBrainPrime = () => {
   const instruction = 'Answer "yes" if given number is prime. Otherwise answer "no".';
-  const prepareRound = () => {
-    const question = getRandomNumber(1, 100);
-
-    const isPrime = (number) => {
-      let divisorsCount = 0;
-      if (number < 2) {
-        divisorsCount += 1;
-      }
-      for (let i = 2; i <= Math.sqrt(question); i += 1) {
-        if (question % i === 0) {
-          divisorsCount += 1;
-          break;
-        }
-      }
-      return divisorsCount === 0;
-    };
-    const correctAnswer = (isPrime(question)) ? 'yes' : 'no';
-
-    return [question, correctAnswer];
-  };
 
   playBrainGames(instruction, prepareRound);
 };
