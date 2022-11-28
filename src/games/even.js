727ef9e2c6af6c0aae47369d3082d732483cdfb0
OksanaS13 <oksan4ik_96@mail.ru>
2022-11-28 17:40:23 +0300

    update src/games/even.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,15 +1,17 @@
 import playBrainGames from '../index.js';
 import getRandomNumber from '../utils.js';
 
+const isEven = (number) => (number % 2 === 0);
+
+const prepareRound = () => {
+  const question = getRandomNumber(1, 100);
+  const correctAnswer = (isEven(question)) ? 'yes' : 'no';
+
+  return [question, correctAnswer];
+};
+
 const playBrainEven = () => {
   const instruction = 'Answer "yes" if the number is even, otherwise answer "no".';
-  const prepareRound = () => {
-    const question = getRandomNumber(1, 100);
-
-    const isEven = (number) => (number % 2 === 0);
-    const correctAnswer = (isEven(question)) ? 'yes' : 'no';
-    return [question, correctAnswer];
-  };
 
   playBrainGames(instruction, prepareRound);
 };
