9d85f65613d7a4c07ec00c2e910542c56fc96b62
OksanaS13 <oksan4ik_96@mail.ru>
2022-11-28 17:41:25 +0300

    update src/games/progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,39 +1,39 @@
 import playBrainGames from '../index.js';
 import getRandomNumber from '../utils.js';
 
+const createProgression = (length, start, incr) => {
+  const progression = [start];
+  let nextNumber = start;
+  for (let i = 1; i < length; i += 1) {
+    nextNumber += incr;
+    progression.push(nextNumber);
+  }
+  return progression;
+};
+
+const prepareRound = () => {
+  const firstNumber = getRandomNumber();
+  const progressionLength = getRandomNumber(5, 10);
+  const step = getRandomNumber(2, 5);
+  const hiddenElem = getRandomNumber(0, progressionLength);
+
+  const progression = createProgression(progressionLength, firstNumber, step);
+
+  const createQuestion = (sequence, num) => {
+    const clonProgression = sequence.slice();
+    clonProgression[num] = '..';
+    return clonProgression.join(' ');
+  };
+
+  const question = createQuestion(progression, hiddenElem);
+
+  const correctAnswer = progression[hiddenElem];
+
+  return [question, String(correctAnswer)];
+};
+
 const playBrainProgression = () => {
   const instruction = 'What number is missing in the progression?';
-
-  const prepareRound = () => {
-    const firstNumber = getRandomNumber();
-    const progressionLength = getRandomNumber(5, 10);
-    const step = getRandomNumber(2, 5);
-    const hiddenElem = getRandomNumber(0, progressionLength);
-
-    const createAProgression = (length, start, incr) => {
-      const progression = [start];
-      let nextNumber = firstNumber;
-      for (let i = 1; i < length; i += 1) {
-        nextNumber += incr;
-        progression.push(nextNumber);
-      }
-      return progression;
-    };
-
-    const progression = createAProgression(progressionLength, firstNumber, step);
-
-    const createAQuestion = (sequence, num) => {
-      const clonProgression = sequence.slice();
-      clonProgression[num] = '..';
-      return clonProgression.join(' ');
-    };
-
-    const question = createAQuestion(progression, hiddenElem);
-
-    const correctAnswer = progression[hiddenElem];
-
-    return [question, String(correctAnswer)];
-  };
 
   playBrainGames(instruction, prepareRound);
 };
