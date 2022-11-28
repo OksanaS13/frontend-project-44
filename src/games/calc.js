c05d96011511f70d65d6b882a3fc4af0a1e3c05d
OksanaS13 <oksan4ik_96@mail.ru>
2022-11-28 17:39:11 +0300

    update src/games/calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,32 +1,33 @@
 import playBrainGames from '../index.js';
 import getRandomNumber from '../utils.js';
 
+const getResultOfExpression = (operator, numOne, numTwo) => {
+  switch (operator) {
+    case '+':
+      return numOne + numTwo;
+    case '-':
+      return numOne - numTwo;
+    case '*':
+      return numOne * numTwo;
+    default:
+      throw new Error(`Unknown operator: '${operator}'!`);
+  }
+};
+
+const prepareRound = () => {
+  const numberOne = getRandomNumber(0, 20);
+  const numberTwo = getRandomNumber(0, 20);
+  const operations = ['+', '-', '*'];
+  const operator = operations[getRandomNumber(0, operations.length)];
+  const question = `${numberOne} ${operator} ${numberTwo}`;
+
+  const correctAnswer = getResultOfExpression(operator, numberOne, numberTwo);
+
+  return [question, String(correctAnswer)];
+};
+
 const playBrainCalc = () => {
   const instruction = 'What is the result of the expression?';
-
-  const prepareRound = () => {
-    const numberOne = getRandomNumber(0, 20);
-    const numberTwo = getRandomNumber(0, 20);
-    const operations = ['+', '-', '*'];
-    const operator = operations[getRandomNumber(0, operations.length)];
-    const question = `${numberOne} ${operator} ${numberTwo}`;
-
-    const getTheResultOfExpression = (symbol, numOne, numTwo) => {
-      switch (symbol) {
-        case '+':
-          return numOne + numTwo;
-        case '-':
-          return numOne - numTwo;
-        case '*':
-          return numOne * numTwo;
-        default:
-          throw new Error(`Unknown operator: '${symbol}'!`);
-      }
-    };
-    const correctAnswer = getTheResultOfExpression(operator, numberOne, numberTwo);
-
-    return [question, String(correctAnswer)];
-  };
 
   playBrainGames(instruction, prepareRound);
 };
